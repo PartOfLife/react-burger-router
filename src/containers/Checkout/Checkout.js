e2ad6cdf759e857aa21bc64712ac5c362e2a3e54
DatNT <nguyendatpm2@gmail.com>
2020-01-09 17:29:26 +0700

    lecture 11

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import CheckoutSumary from './../../components/Order/CheckoutSumary/CheckoutSumary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 1,
-      meat: 1
-    }
+    ingredients: null,
+    totalPrice: 0
   }
 
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search)
     const ingredients = {};
+    let price = 0;
     for (let param of query.entries()) {
-      ingredients[param[0]] = +param[1]
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1]
+      }
+
     }
-    this.setState({ingredients});
+    this.setState({ingredients, totalPrice: price});
   }
 
   checkoutCancelled = () => {
@@ -30,10 +34,14 @@ class Checkout extends Component {
 
   render() {
     return (
-      <CheckoutSumary
+      <div>
+        <CheckoutSumary
         ingredients={this.state.ingredients}
         checkoutCancelled={this.checkoutCancelled}
         checkoutContinued={this.checkoutContinued} />
+        <Route path={this.props.match.path + '/contact-data'}
+          render={() => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...this.props} />)} />
+      </div>
     )
   }
 }
---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Order from './../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
+
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+  componentDidMount() {
+    axios.get('/orders')
+      .then(res => {
+        const featchedOrders = [];
+        for (let key in res.data) {
+          featchedOrders.push({...res.data[key], id: key})
+        }
+        this.setState({loading: false, orders: featchedOrders});
+      })
+      .catch(err => {
+        this.setState({loading: false});
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
+  };
+}
+
+export default withErrorHandler(Orders, axios);
